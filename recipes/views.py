6eec3ca2de060da707cfcc57c41aeb39e95345e0
Julia Ayres <j.bobrineva@gmail.com>
2021-07-24 13:55:29 -0400

    added filtering recipes function, thankyou html form

--- recipes/views.py
+++ recipes/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Customer
 from .forms import RecipeForm
 import requests
+import re
 
 RECIPE_BASE_URL = "https://60f5adf918254c00176dffc8.mockapi.io/api/v1/recipes/"
 
@@ -27,5 +28,18 @@ def home(request):
     return render(request, 'recipes/home.html', {'form': form})
 
 
-def recipe_list(request):
-    return render(request, 'recipes/thankyou.html')
+def recipe_list(request, id):
+    customer = get_object_or_404(Customer, pk=id)
+    # regex to get rid of spec char in allergy str
+    cust_allrg = re.sub('[^a-zA-Z,]+', '', customer.allergies)
+    responses = requests.get(RECIPE_BASE_URL)
+    if responses.status_code == 200:
+        recipes = responses.json()
+        filtered_recipes = [
+            recipe for recipe in recipes if (recipe['allergens'] == [] or recipe['allergens'][0] not in customer.allergies)]
+    db_recipe_list = []
+    for item in filtered_recipes:
+        db_recipe_list.append(item['name'])
+    customer.recipes = db_recipe_list
+    customer.save()
+    return render(request, 'recipes/thankyou.html', {'cust_allrg': cust_allrg, 'customer': customer, 'filtered_recipes': filtered_recipes})
