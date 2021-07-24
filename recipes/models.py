6d851a940090628b7c6b4a7bad3f5986141532a7
Julia Ayres <j.bobrineva@gmail.com>
2021-07-24 13:26:05 -0400

    added Customer model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=250)
+    child_first_name = models.CharField(max_length=50)
+    child_last_name = models.CharField(max_length=50)
+    allergies = models.CharField(max_length=300)
+    recipes = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.email
