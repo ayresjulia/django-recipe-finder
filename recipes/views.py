63c891c84af7640887e9674077194fc767b7baa1
Julia Ayres <j.bobrineva@gmail.com>
2021-07-24 13:27:13 -0400

    updates tinyorganics/urls and written home function to get info from the form

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,  redirect, get_object_or_404
+from django.http import HttpResponse
+from .models import Customer
+from .forms import RecipeForm
+import requests
 # Create your views here.
